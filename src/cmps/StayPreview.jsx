b6f6504131bea06a31f324ffae20ad059584bfad
Yuval Kellner <yukellner@gmail.com>
2022-05-25 13:54:57 +0300

    gallery design start

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,10 +1,12 @@
+// import { StayPreview } from "../assets/img/
+
 export function StayPreview({stay}) {
 
-
-    
+    // let url  = '../assets/img/'+stay.imgUrls[0]
+    // console.log('url',url)
     return (
         <div className="stay-card">
-            <img src={stay.imgUrls[0]} ></img>
+            <img src={require('../assets/img/'+stay.imgUrls[0])}></img>
             <h1>{stay.name}</h1>
         </div>
     )
