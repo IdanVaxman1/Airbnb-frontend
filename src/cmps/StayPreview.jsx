c3aacb966d92e92830c4b91a4a124abe9a8b063e
Yuval Kellner <yukellner@gmail.com>
2022-05-25 17:03:09 +0300

    somw changes

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,14 +1,21 @@
 // import { StayPreview } from "../assets/img/
+// import 'font-awesome/css/font-awesome.min.css';
+
+
 
 export function StayPreview({stay}) {
 
-    // let url  = '../assets/img/'+stay.imgUrls[0]
-    // console.log('url',url)
+    
+    
     return (
         <div className="stay-card">
+            <i class="fa-solid fa-arrow-left"></i>
+            <i class="fa-solid fa-arrow-right"></i>
             <img src='https://res.cloudinary.com/dhy6ndeij/image/upload/v1653480425/001_urftcv.jpg'></img>
             {/* <img src={require('../assets/img/'+stay.imgUrls[0])}></img> */}
             <h1>{stay.name}</h1>
         </div>
     )
+
 }
+
