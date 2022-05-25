0d0019410b6be16987e0a624671761733af20d66
Yuval Kellner <yukellner@gmail.com>
2022-05-25 15:15:22 +0300

    update 1 image

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -6,7 +6,8 @@ export function StayPreview({stay}) {
     // console.log('url',url)
     return (
         <div className="stay-card">
-            <img src={require('../assets/img/'+stay.imgUrls[0])}></img>
+            <img src='https://res.cloudinary.com/dhy6ndeij/image/upload/v1653480425/001_urftcv.jpg'></img>
+            {/* <img src={require('../assets/img/'+stay.imgUrls[0])}></img> */}
             <h1>{stay.name}</h1>
         </div>
     )
