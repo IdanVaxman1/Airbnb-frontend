85d071198b5b2b16cdf089371868b9b65265f27b
Idan Vaxman <idanvaxman@icloud.com>
2022-05-25 15:41:05 +0300

    .

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -6,7 +6,7 @@ export function StayPreview({stay}) {
     // console.log('url',url)
     return (
         <div className="stay-card">
-            <img src={require('../assets/img/'+stay.imgUrls[0])}></img>
+            {/* <img src={require('../assets/img/'+stay.imgUrls[0])}></img> */}
             <h1>{stay.name}</h1>
         </div>
     )
--- src/pages/stayDetails.jsx
+++ src/pages/stayDetails.jsx
@@ -1,28 +1,25 @@
 import { useEffect, useState } from "react"
 import { storageService } from "../services/async-storage.service"
+import { useParams } from "react-router-dom";
 
 
 
 export const StayDetails = () => {
 
     const [stays, setStays] = useState(null)
-  
+    const { id } = useParams();
+
+
     useEffect(() => {
-        getStays()
+        
     }, [])
     
-    const getStays = async () => {
-        const stays = await storageService.query()
-        setStays(stays)
-        console.log('details' , stays);
-    }
-
     return <div className="center">
         <h2>details</h2>
-        <h2>{stays[0].name}</h2> 
+        {/* <h2>{stays[0].name}</h2>  */}
         <ul>
-            <li>{stays[0].address.street}</li>
-            <li>({stays[0].reviews.length} reviews)</li>
+            {/* <li>{stays[0].address.street}</li>
+            <li>({stays[0].reviews.length} reviews)</li> */}
             <li></li>
         </ul>
     </div>
