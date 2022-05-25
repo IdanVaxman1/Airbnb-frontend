1825e3d06b1c0c084f9e649ebaa0cf86d17bd397
idan berger <idanberger22@gmail.com>
2022-05-25 11:04:09 +0300

    fff

--- src/cmps/main-filter.jsx
+++ src/cmps/main-filter.jsx
@@ -2,14 +2,19 @@ import { useEffect, useRef, useState } from "react"
 import { NavLink } from "react-router-dom";
 export const MainFilter = () => {
     const [isHome, setIsHome] = useState(true)
+    const [byLocation,setLocation] = useState('')
+    const [byStart,setStart] = useState('2022-04-18')
+    const [byEnd,setEnd] = useState('2022-04-22')
+
+    
     return (
-        <div className="center-h" style={{ background: 'white', width: '800px', top: '500px' }}>
+        <div className="center-h" style={{position:'fixed', background: 'white', width: '800px', top: '300px', left:'300px'}}>
 
             <input name="location" type="text" placeholder="Location" />
 
             <input type="date" id="start" name="trip-start"
                 value="2022-04-18"
-                min="202022-01-01" max="2018-12-31"></input>
+                min="2022-01-01" max="2022-12-31"></input>
 
             <input type="date" id="end" name="trip-end"
                 value="2022-04-22"
