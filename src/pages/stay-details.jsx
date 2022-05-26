98ef1f4558a4085735e30060bcee7ced6c74ddf7
Idan Vaxman <idanvaxman@icloud.com>
2022-05-26 17:10:57 +0300

    changes

--- src/pages/stay-details.jsx
+++ src/pages/stay-details.jsx
@@ -11,7 +11,7 @@ export const StayDetails = () => {
 
 
     useEffect(() => {
-        getStay()      
+        getStay()
     }, [])
 
     const getStay = async () => {
@@ -20,11 +20,33 @@ export const StayDetails = () => {
     }
 
 
-        { if (!stay) return (<h1>loading</h1>) }
+    { if (!stay) return (<h1>loading</h1>) }
 
-    return <div className="">
+    return <div className="details-container">
         <h2>{stay.name}</h2>
-        
+        <section className="details-header">
+            <section className="flex">
+                <h4>{stay.reviewScores.value / 2}<span class="material-icons">star</span></h4>
+                <h4>({stay.reviews.length} reviews)</h4>
+                <h4>{stay.address.street}</h4>
+                <h4></h4>
+            </section>
+            <section className="flex">
+                <h4>Share</h4>
+                <h4>Save</h4>
+            </section>
+        </section>
+        {/* <DetailsGallery /> */}
+        <section>
+            <h2>{stay.name}</h2>
+            <h5>{stay.capacity} guest · {stay.bedrooms} bedrooms · {stay.beds} beds · {stay.bathrooms} baths </h5>
+        </section>
+        <section>
+            <div><span class="material-icons-outlined"></span></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
     </div>
 
 
