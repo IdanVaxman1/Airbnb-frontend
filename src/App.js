b1fa1e51c5f6d05a604db3c52ab0a20b48489486
Yuval Kellner <yukellner@gmail.com>
2022-05-26 12:58:38 +0300

    add initial storage data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { AppHeader } from './cmps/app-header'
 import {AppFooter} from './cmps/app-footer'
-import {Home} from './pages/Home.jsx'
-import {Explore} from './pages/Explore'
+import {Home} from './pages/home.jsx'
+import {Explore} from './pages/explore'
 import { Login } from './pages/login'
 import {StayDetails} from './pages/stay-details'
 
