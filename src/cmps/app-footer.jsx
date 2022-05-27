a986c8b454ae1d5a0f1428ebbc66866dc311c3ff
Yuval Kellner <yukellner@gmail.com>
2022-05-27 18:55:43 +0300

    footer design

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -1,8 +1,29 @@
 export function AppFooter() {
-    
+
     return (
-        <div className="center">
-            <h1>cofferights 2022</h1>
+        <div className="footer">
+            <div className="left-side">
+                <li><h1>cofferights 2022</h1></li>
+                ·
+                <li><h1>Privacy</h1></li>
+                ·
+                <li><h1>Terms</h1></li>
+                ·
+                <li><h1>Sitemap</h1></li>
+                
+                
+
+            </div>
+            <div className="right-side">
+                <li>
+                <span class="material-icons">language</span>
+                </li>
+                <li>English(US)</li>
+                ·
+                <li>$ USD</li>
+                ·
+                <li>Support & resources</li>
+            </div>
         </div>
     )
 }
