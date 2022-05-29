b35f27f26e8854597b24cf617f42ada1be271f74
Idan Vaxman <idanvaxman@icloud.com>
2022-05-29 13:28:02 +0300

    some changes

--- src/cmps/stay-deatails-bellow.jsx
+++ src/cmps/stay-deatails-bellow.jsx
@@ -23,14 +23,16 @@ export function StayDeatailsBellow({ stay }) {
             <section>
                 <h5>{stay.capacity} guest · {stay.bedrooms} bedrooms · {stay.beds} beds · {stay.bathrooms} baths </h5>
             </section>
+
+           
             <div className="feature-block">
                 <div className="feature-logo"><span className="material-icons">home</span></div>
                 <section className="paragraph">
                     <div>
-                        <h4>Entire home</h4>
+                        <h4>Designed by</h4>
                     </div>
                     <div>
-                        <h5>You’ll have the apartment to yourself.</h5>
+                        <h5>Meredith Higgins & Bryant GingerichRenee Byler, Burrow Creative</h5>
                     </div>
                 </section>
             </div>
@@ -38,50 +40,32 @@ export function StayDeatailsBellow({ stay }) {
                 <div className="feature-logo"><span className="material-icons ">location_on</span></div>
                 <section className="paragraph">
                     <div>
-                        <h4>Great location</h4>
+                        <h4>Self check-in</h4>
                     </div>
                     <div>
-                        <h5>Recent guests gave the location a 5-star rating.</h5>
+                        <h5>Check yourself in with the lockbox.</h5>
                     </div>
                 </section>
             </div>
             <div className="feature-block">
                 <div className="feature-logo"><span className="material-icons">auto_awesome</span></div>
                 <section className="paragraph">
-                    <div>
-                        <h4>Enhanced Clean</h4>
-                    </div>
-                    <div>
-                        <h5>This host has committed to our 5-step enhanced cleaning process.</h5>
-                    </div>
-                </section>
+                    <div><h4>Free cancellation for 48 hours.</h4></div></section>
             </div>
 
-            <div className="feature-block">
-                <div className="feature-logo"><span className="material-icons">event</span></div>
-                <section className="paragraph">
-                    <div>
-                        <h4>Free cancellation up to 24 hours before check-in</h4>
-                    </div>
-                    <div>
-                        <h5>feel free to be flexible.</h5>
-                    </div>
-                </section>
-            </div>
-
-
+            <div className="_npr0qi"></div>
 
             <div className="stay-cover">
                 <h1><span className="stay-cover-logo">stay</span>cover</h1>
                 <div className="stay-cover-text">
                     <p>Every booking includes free protection from Host cancellations, listing inaccuracies,
-                        and  other issues like trouble checking in.<br /> Learn more</p>
+                        and  other issues like trouble checking in.<br /> <p className="stay-cover-more">Learn more</p></p>
                 </div>
             </div>
 
             <div className="stay-description">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non unde totam nostrum error corporis quis velit dicta sapiente vitae ut, delectus repudiandae iste sit excepturi quidem nulla odit earum facere a aperiam voluptates blanditiis,<br /> pariatur in voluptatibus! Repellat placeat quam voluptate possimus qui, error earum! Aut asperiores fugiat illum rem omnis <br />qui nam aliquid, veniam saepe quaerat accusamus porro vero, tenetur quia repellat? Nihil blanditiis nulla architecto vero quaerat unde accusamus vitae. Voluptatibus laborum odio odit omnis repellendus? Dolore,<br /> commodi! Veritatis, provident. Quasi officia tempore, temporibus, magnam recusandae optio ullam accusantium provident placeat ea laboriosam aliquam inventore non minus culpa.
-                <p>Show more</p>
+                <p className="stay-cover-more">Show more</p>
             </div>
         </section>
 
