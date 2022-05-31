eab32d572daa44490c71c52fae3c2f51179f0a26
Yuval Kellner <yukellner@gmail.com>
2022-05-31 20:15:53 +0300

    login dropdown improved

--- src/cmps/user-menu-modal.jsx
+++ src/cmps/user-menu-modal.jsx
@@ -17,11 +17,11 @@ export const UserMenuModal = (props) => {
     }
 
     return (<section className="user-menu-container">
-        <li>
-            <p onClick={() => toggleModal(false)} className="clickable noselect">Sign up</p>
+        <li onClick={() => toggleModal(false)} className="clickable noselect">
+            <p >Sign up</p>
         </li>
-        <li>
-            <p onClick={() => toggleModal(true)} className="clickable noselect" >Log in</p>
+        <li onClick={() => toggleModal(true)} className="clickable noselect">
+            <p  >Log in</p>
         </li>
         <li>
             <NavLink className='undecorate' onClick={closeSelf} to='/login' >Host your home</NavLink>
