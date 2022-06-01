c9619f5b870661f0b501aa25b3517f560fa9bdff
idan berger <idanberger22@gmail.com>
2022-06-01 14:57:49 +0300

    basic users

--- src/cmps/user-menu-modal.jsx
+++ src/cmps/user-menu-modal.jsx
@@ -1,31 +1,36 @@
 import { NavLink } from "react-router-dom"
 import { useDispatch } from 'react-redux'
 import { openModal } from "../store/actions/userActions"
+import { userService } from "../services/user.service"
 
 
 export const UserMenuModal = (props) => {
 
     const dispatch = useDispatch()
+    const loggedinUser = userService.getLoggedinUser()
 
     const toggleModal = (isLogin) => {
         dispatch(openModal(isLogin))
         closeSelf()
     }
 
-    const closeSelf = ()=>{
+    const closeSelf = () => {
         props.toggleModal()
     }
 
     return (<section className="user-menu-container">
-        <li onClick={() => toggleModal(false)} className="clickable noselect">
+        {!loggedinUser && <li onClick={() => toggleModal(false)} className="clickable noselect">
             <p >Sign up</p>
-        </li>
-        <li onClick={() => toggleModal(true)} className="clickable noselect">
+        </li>}
+        {!loggedinUser && <li onClick={() => toggleModal(true)} className="clickable noselect">
             <p  >Log in</p>
-        </li>
+        </li>}
         <li>
             <NavLink className='undecorate' onClick={closeSelf} to='/login' >Host your home</NavLink>
         </li>
+        {loggedinUser && <li>
+            <p onClick={userService.logout}>Log out</p>
+        </li>}
     </section>
     )
 }
