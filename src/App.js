b5de11384f95cd1abb6c49987f9156866555abb1
Yuval Kellner <yukellner@gmail.com>
2022-06-01 23:31:52 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
                     </Routes>
                     <LoginSignup/>
                 </main>
-              <AppFooter/>
+              <AppFooter />
             </div>
         </Router>
   );
--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -1,32 +1,33 @@
 export function AppFooter() {
 
     return (
-        <div className="fixed footer">
-             
+        
+            <div className="fixed footer ">
 
-            <div className="left-side">
-                <li><h1>cofferights 2022</h1></li>
-                <li>·</li>
-                <li><h1>Privacy</h1></li>
-                <li>·</li>
-                <li><h1>Terms</h1></li>
-                <li>·</li>
-                <li><h1>Sitemap</h1></li>
-                
-                
+                <div className="left-side">
+                    <li><h1>cofferights 2022</h1></li>
+                    <li>·</li>
+                    <li><h1>Privacy</h1></li>
+                    <li>·</li>
+                    <li><h1>Terms</h1></li>
+                    <li>·</li>
+                    <li><h1>Sitemap</h1></li>
 
-            </div>
-            <div className="right-side">
-                <li>
-                <span className="material-icons">language</span>
-                </li>
-                <li>English(US)</li>
-                  <li>·</li>
-                <li>$ USD</li>
-                  <li>·</li>
-                <li>Support & resources</li>
-            </div>
-             
+
+
+                </div>
+                <div className="right-side">
+                    <li>
+                        <span className="material-icons">language</span>
+                    </li>
+                    <li>English(US)</li>
+                    <li>·</li>
+                    <li>$ USD</li>
+                    <li>·</li>
+                    <li>Support & resources</li>
+                </div>
+           
         </div>
+      
     )
 }
