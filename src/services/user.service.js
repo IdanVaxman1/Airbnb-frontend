4f8036cde04179e5d40608cfe7207f34765a80f9
idan berger <idanberger22@gmail.com>
2022-06-01 13:54:35 +0300

    wider login

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,7 +4,6 @@ import { storageService } from './async-storage.service'
 // import { socketService, SOCKET_EVENT_USER_UPDATED, SOCKET_EMIT_USER_WATCH } from './socket.service'
 
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
-// var gWatchedUser = null;
 
 export const userService = {
     login,
@@ -14,9 +13,6 @@ export const userService = {
     saveLocalUser,
     getUsers,
     getById,
-    // remove,
-    // update,
-    // changeScore
 }
 
 window.userService = userService
@@ -43,18 +39,6 @@ async function getById(userId) {
 
     return user
 }
-// function remove(userId) {
-//     return storageService.remove('user', userId)
-//     // return httpService.delete(`user/${userId}`)
-// }
-
-// async function update(user) {
-//     await storageService.put('user', user)
-//     // user = await httpService.put(`user/${user._id}`, user)
-//     // Handle case in which admin updates other user's details
-//     if (getLoggedinUser()._id === user._id) saveLocalUser(user)
-//     return user;
-// }
 
 async function login(userCred) {
     const users = await storageService.queryUser('user')
@@ -77,15 +61,6 @@ async function logout() {
     // socketService.logout()
     // return await httpService.post('auth/logout')
 }
-
-// async function changeScore(by) {
-//     const user = getLoggedinUser()
-//     if (!user) throw new Error('Not loggedin')
-//     user.score = user.score + by || by
-//     await update(user)
-//     return user.score
-// }
-
 
 function saveLocalUser(user) {
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
