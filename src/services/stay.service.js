af8e1d9abf75c435a0b6cd4d039604dbe623a156
idan berger <idanberger22@gmail.com>
2022-06-02 14:31:28 +0300

    fff

--- src/services/stay.service.js
+++ src/services/stay.service.js
@@ -1,70 +1,51 @@
 import { storageService } from './async-storage.service.js'
+import { dataService } from './stay.data.js'
 import { utilService } from './util.service.js'
-// import { userService } from './user.service.js'
-// import { getActionRemoveStay, getActionAddStay, getActionUpdateStay } from '../store/stay.actions.js'
+import { httpService } from './http.service.js'
 
 const STORAGE_KEY = 'stay'
-// const stayChannel = new BroadcastChannel('stayChannel')
-// const listeners = []
 
 export const stayService = {
     query,
     getById,
-    // save,
-    // remove,
-    // subscribe,
-    // unsubscribe
+    getTopRated,
+    getRandomStayId,
 }
 window.cs = stayService;
 
-
-async function query(filterBy,exploreFilterBy) {
-    let stays = await storageService.query(STORAGE_KEY)
-    if(filterBy){
-    if(filterBy.location) stays=stays.filter(stay=>stay.address.country.includes(utilService.capitalizeFirst(filterBy.location))||
-    stay.address.city.includes(utilService.capitalizeFirst(filterBy.location)))
-}
-    if(exploreFilterBy){
-        stays=stays.filter(stay=>stay.price<=exploreFilterBy.maxPrice && stay.price>=exploreFilterBy.minPrice)
-        if(exploreFilterBy.roomType){
-            stays=stays.filter(stay=>stay.roomType===exploreFilterBy.roomType)
+async function query(filterBy, exploreFilterBy) {
+    let stays = await httpService.get('stay',filterBy)
+    if (exploreFilterBy) {
+        stays = stays.filter(stay => stay.price <= exploreFilterBy.maxPrice && stay.price >= exploreFilterBy.minPrice)
+        if (exploreFilterBy.roomTypes) {
+            const fullRoomTypes = ['Entire home/apt', 'Hotel room', 'Private room', 'Shared room']
+            fullRoomTypes.forEach(type => {
+                if (!exploreFilterBy.roomTypes.includes(type)) stays = stays.filter(stay => stay.roomType !== type)
+            })
         }
-        if(exploreFilterBy.amenities){
-            exploreFilterBy.amenities.forEach(amn=>{stays=stays.filter(stay=>stay.amenities.includes(amn))})
+        if (exploreFilterBy.amenities) {
+            exploreFilterBy.amenities.forEach(amn => { stays = stays.filter(stay => stay.amenities.includes(amn)) })
         }
     }
     return stays
 }
-function getById(stayId) {
+async function getById(stayId) {
     return storageService.get(STORAGE_KEY, stayId)
-    // return axios.get(`/api/stay/${stayId}`)
+    // return await httpService.get(`stay/${stayId}`)
 }
-// async function remove(stayId) {
-//     // return new Promise((resolve, reject) => {
-//     //     setTimeout(reject, 2000)
-//     // })
-//     // return Promise.reject('Not now!');
-//     await storageService.remove(STORAGE_KEY, stayId)
-//     stayChannel.postMessage(getActionRemoveStay(stayId))
-// }
-// async function save(stay) {
-//     var savedStay
-//     if (stay._id) {
-//         savedStay = await storageService.put(STORAGE_KEY, stay)
-//         stayChannel.postMessage(getActionUpdateStay(savedStay))
-        
-//     } else {
-//         // Later, owner is set by the backend
-//         stay.owner = userService.getLoggedinUser()
-//         savedStay = await storageService.post(STORAGE_KEY, stay)
-//         stayChannel.postMessage(getActionAddStay(savedStay))
-//     }
-//     return savedStay
-// }
 
-// function subscribe(listener) {
-//     stayChannel.addEventListener('message', listener)
-// }
-// function unsubscribe(listener) {
-//     stayChannel.removeEventListener('message', listener)
-// }
+async function addStay(stay) {
+    // const addedStay = await httpService.post('stay', stay)
+    // return addedStay
+}
+
+
+function getTopRated() {
+    return dataService.getTopRated()
+}
+
+async function getRandomStayId() {
+    const stays = await storageService.query()
+    const idx = utilService.getRandomIntInclusive(0, stays.length - 1)
+    return stays[idx]._id
+}
