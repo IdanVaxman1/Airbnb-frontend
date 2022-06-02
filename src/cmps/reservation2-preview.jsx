366e8eb1022db938afa3a365016c28d962376f7a
Yuval Kellner <yukellner@gmail.com>
2022-06-02 14:20:36 +0300

    some updates

--- src/cmps/reservation2-preview.jsx
+++ src/cmps/reservation2-preview.jsx
@@ -4,32 +4,36 @@ import { Link } from 'react-router-dom'
 import { useEffect, useRef, useState } from "react"
 
 export function ReservationPreview2({ reservation }) {
-    
-    {console.log('reservation',reservation)}
 
-    
+    { console.log('reservation', reservation) }
+    var date = reservation.checkIn
+
+
 
     return (
-            <div className="reservation-card">
+        <div className="reservation-card box-shadow">
+            <div>
+
                 <div>
-                    <div>
+                    <h2>Property:</h2>
+                    <h3>{reservation.stay.name}</h3>
                     <h2>checkIn:</h2>
                     <h3>
-                    {reservation.checkIn}
+                        {reservation.checkIn}
                     </h3>
                     {/* .toISOString().split('T')[0] */}
-                    </div>
-                    <div>
+                </div>
+                <div>
                     <h2>checkOut:</h2>
                     <h3>
-                    {reservation.checkOut}
+                        {reservation.checkOut}
                     </h3>
-                    </div>
-                    
-               
                 </div>
+
+
             </div>
-       
+        </div>
+
     )
 
 }
